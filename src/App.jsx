f30f1b0b1565ee88be9de3dc93a1ebaca125f606
Igor Shabat <shabat.igor@gmail.com>
2016-11-20 17:17:44 +0200

    Added dependencies and changed structure of folders

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Main from './screens/Main';
+import './assets/stylesheets/index.css';
+
+ReactDOM.render(
+  <Main />,
+  document.getElementById('root'),
+);
