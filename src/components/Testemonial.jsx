768d27c70fc8a14adf76a8b2836f3f00ce4ac6a4
Igor Shabat <shabat.igor@gmail.com>
2017-05-10 17:47:56 +0300

    Fix for testemonials

--- src/components/Testemonial.jsx
+++ src/components/Testemonial.jsx
@@ -3,33 +3,38 @@ import React from 'react';
 
 function Testemonial({ children, signedBy, company, url, logo, className }) {
   return (
-    <section className={`testemonial-panel${className ? ' ' + className : ''}`}>
-      <div className="testemonial-logo">
-        <a
-          className="testemonial-link"
-          href={url}
-          role="link"
-          target="_blank"
-        >
-          { logo ? <img src={logo} role="presentation" /> : <h2>{company}</h2> }
-        </a>
-      </div>
-      <div className="testemonial-text">
-        <blockquote cite={url}>
-          {children}
-          <br />
-          <span>
-            - {signedBy}
-            &nbsp;
-            <a
-              className="testemonial-link"
-              href={url}
-              role="link"
-              target="_blank"
-            >{company}</a>
-          </span>
-        </blockquote>
-      </div>
+    <section className="testemonial-wrapper">
+      <section className={`testemonial-panel${className ? ' ' + className : ''}`}>
+        <div className="testemonial-logo">
+          <a
+            className="testemonial-link"
+            href={url}
+            role="link"
+            target="_blank"
+          >
+            { logo ? <img src={logo} role="presentation" /> : <h2>{company}</h2> }
+          </a>
+        </div>
+        <div className="testemonial-text">
+          <blockquote cite={url}>
+            <section className="testemonial-body">
+              <span>{children}</span>
+            </section>
+            <section className="testemonial-sign">
+              <span>
+                - {signedBy}
+                &nbsp;
+                <a
+                  className="testemonial-link"
+                  href={url}
+                  role="link"
+                  target="_blank"
+                >{company}</a>
+              </span>
+            </section>
+          </blockquote>
+        </div>
+      </section>
     </section>
   );
 }
--- src/screens/Projects/index.jsx
+++ src/screens/Projects/index.jsx
@@ -1,6 +1,5 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import MediaQuery from 'react-responsive';
 
 import Content from '../../components/Content';
 import Testemonial from '../../components/Testemonial';
@@ -30,16 +29,6 @@ class Projects extends React.Component {
         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong>UpWave</strong> is a great engineering partner thanks to their depth of knowledge in technology, their design experience, coupled with their flexibility to engage with their clients and their firm commitment to deliver. <strong>UpWave</strong> team always tries to learn and understand our business needs instead of just coding to our specs.
       </Testemonial>,
       <Testemonial
-        key="ourea"
-        className="ourea"
-        signedBy="James Campion, CEO at"
-        company="Ourea"
-        url="http://oureasystems.com/"
-        logo={ourea}
-      >
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong>UpWave agency</strong> helped develop, design and maintain our webapp. I found their service to be very friendly and helpful. They will always go out of their way to ensure completion of tasks in the most effective way. <strong>UpWave</strong> completed work quickly and always to the standard you expect. I would recommend <strong>UpWave agency's</strong> service to anyone looking for talented developers.
-      </Testemonial>,
-      <Testemonial
         key="eventgeek"
         className="eventgeek"
         signedBy="Alex Patriquin, CEO at"
@@ -48,6 +37,16 @@ class Projects extends React.Component {
         logo={eventgeek}
       >
         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;We had a great experience working together with <strong>UpWave agency</strong>. Agency made solid efforts and their contributions meaningfully advanced our company's progress. Their greatest strength is in thinking about both technical and business problems holistically.
+      </Testemonial>,
+      <Testemonial
+        key="ourea"
+        className="ourea"
+        signedBy="James Campion, CEO at"
+        company="Ourea"
+        url="http://oureasystems.com/"
+        logo={ourea}
+      >
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong>UpWave agency</strong> helped develop, design and maintain our webapp. I found their service to be very friendly and helpful. They will always go out of their way to ensure completion of tasks in the most effective way. <strong>UpWave</strong> completed work quickly and always to the standard you expect. I would recommend <strong>UpWave agency's</strong> service to anyone looking for talented developers.
       </Testemonial>,
     ];
   }
@@ -60,16 +59,9 @@ class Projects extends React.Component {
           registerAnimation={this.props.registerAnimation}
           className="box our-work"
         >
-          <MediaQuery query='(max-device-width: 568px)'>
-            <section className='testimonials-container'>
-              {this.testemonials[Math.floor(Math.random() * 3)]}
-            </section>
-          </MediaQuery>
-          <MediaQuery query='(min-device-width: 569px)'>
-            <section className='testimonials-container'>
-              {this.testemonials}
-            </section>
-          </MediaQuery>
+          <section className='testimonials-container'>
+            {this.testemonials}
+          </section>
         </Content>
       </section>
     );
